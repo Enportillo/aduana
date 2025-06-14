4ff0b9874ad9d6ba6658aee061467ee36189842c
enportillo <en.portillo@duocuc.cl>
2025-06-14 10:05:04 -0400

    Crear proyecto

---
+++ src/test/java/aduana/example/aduana/AduanaApplicationTests.java
@@ -0,0 +1,13 @@
+package aduana.example.aduana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AduanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
